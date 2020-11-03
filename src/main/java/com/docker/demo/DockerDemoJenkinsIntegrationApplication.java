d30858974aeab4f664cc0e21c5d4bc9fab5b54b8
Smit Patel <smitpatel168@gmail.com>
2020-11-03 12:02:51 -0500

    Initial Commit

---
+++ src/main/java/com/docker/demo/DockerDemoJenkinsIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.docker.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerDemoJenkinsIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerDemoJenkinsIntegrationApplication.class, args);
+	}
+
+}
