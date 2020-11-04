4c48223bd2b90011ed8cacb89a8e0c0f706577da
Smit Patel <smitpatel168@gmail.com>
2020-11-03 23:07:34 -0500

    Added rest method with URL /

--- src/main/java/com/docker/demo/DockerDemoJenkinsIntegrationApplication.java
+++ src/main/java/com/docker/demo/DockerDemoJenkinsIntegrationApplication.java
@@ -2,12 +2,21 @@ package com.docker.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DockerDemoJenkinsIntegrationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DockerDemoJenkinsIntegrationApplication.class, args);
 	}
+	
+	@GetMapping("/")
+	public String testProject() {
+		return "Test Project - Jenkins <=> Docker Integration";
+	}
+	
 
 }
